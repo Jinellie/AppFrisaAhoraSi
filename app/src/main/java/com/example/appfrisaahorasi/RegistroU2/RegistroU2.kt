7f132db61f4e1b1a147d0c81c150b226a3c7bf0e
ChalkyMuffin <95390155+ChalkyMuffin@users.noreply.github.com>
2023-10-03 18:08:51 -0600

    xd

--- app/src/main/java/com/example/appfrisaahorasi/RegistroU2/RegistroU2.kt
+++ app/src/main/java/com/example/appfrisaahorasi/RegistroU2/RegistroU2.kt
@@ -2,8 +2,11 @@ package com.example.appfrisaahorasi.RegistroU2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,11 +15,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Preview
@@ -41,10 +54,39 @@ fun RegistroU2() {
             modifier = Modifier.padding(15.dp))
 
 
+        ExampleBox()
+
+
 
 
 
     }
 
 
+}
+
+//@Preview
+@Composable
+fun ExampleBox(){
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentSize(Alignment.Center)) {
+        Box(
+            modifier = Modifier
+                .height(50.dp)
+                .background(Color.White)
+                .width(275.dp)
+                .border(
+                    width = 1.5.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(15.dp)
+                )
+
+        )
+        {
+            Text(text = "Ambientalistas",
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center)
+        }
+    }
 }
