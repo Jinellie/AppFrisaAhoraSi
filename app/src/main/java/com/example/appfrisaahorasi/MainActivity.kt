e53e4c4932efbdbd81da654c7a99ed360201a44b
Jinellie <89491839+Jinellie@users.noreply.github.com>
2023-10-18 15:49:51 -0600

    nava osc perfil

--- app/src/main/java/com/example/appfrisaahorasi/MainActivity.kt
+++ app/src/main/java/com/example/appfrisaahorasi/MainActivity.kt
@@ -76,7 +76,7 @@ class MainActivity : ComponentActivity() {
                             }
                             // Perfil USUARIO
                             composable(NavRoutes.perfilOrgvistaUsuario) {
-                                PerfilApp("CarlosMex", "Si Somos", "www.instagram.com", "www.twitter.com", "www.facebook.com", navController = controller)
+                                PerfilApp("Diez por Ciénega A.C.", "Nuestra misión en Diez por Ciénega A.C. es promover el desarrollo integral de la población vulnerable en la región de la Ciénega de Chapala. Nos dedicamos a empoderar a las personas y comunidades desfavorecidas, brindándoles acceso a recursos, educación, salud y oportunidades que les permitan alcanzar su máximo potencial. Trabajamos incansablemente para construir un futuro más equitativo y próspero, donde cada individuo tenga las herramientas necesarias para superar desafíos y lograr una vida plena. Juntos, construimos puentes hacia un mejor mañana. #DesarrolloIntegral #Empoderamiento #ComunidadFuerte", "www.instagram.com", "www.twitter.com", "www.facebook.com", navController = controller)
 
                             }
                             composable(NavRoutes.perfilUsuario) {
@@ -132,7 +132,7 @@ class MainActivity : ComponentActivity() {
                             }
                             // HOME
                             composable(NavRoutes.home) {
-                                Home()
+                                Home(navController)
                             }
                             composable(NavRoutes.favoritos) {
                               //  favoritos()
